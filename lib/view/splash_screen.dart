cceec5e03ecc5f3055a34f68fe27d1e842a2c48d
Mustafa Narin <mustafa.narin1132@gmail.com>
2024-07-08 21:51:54 +0300

    Splash screen finished

---
+++ lib/view/splash_screen.dart
@@ -0,0 +1,42 @@
+import 'package:book_store_mobile/product/color/project_colors.dart';
+import 'package:book_store_mobile/product/extensions/build_context_extensions.dart';
+import 'package:book_store_mobile/product/extensions/image_path_extension.dart';
+import 'package:book_store_mobile/product/widgets/elevated_button.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  final String loginButtonText = "Login";
+  final String skipButtonText = "Skip";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: ProjectColors.flyByNight,
+      body: Padding(
+        padding: context.paddingAllLow2,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Spacer(),
+            Center(child: SizedBox(
+              height: context.highLogoHeight,
+              width: context.highLogoHeight,
+              child: Image.asset(ImageName.app_logo.path(),fit: BoxFit.contain,))),
+            const Spacer(),
+            ElevatedButtonProject(text: loginButtonText,onPressed: (){},backgroundColor: ProjectColors.entanRed,textColor: ProjectColors.white,),
+            ElevatedButtonProject(text: skipButtonText,onPressed: (){},backgroundColor: ProjectColors.flyByNight,textColor: ProjectColors.majoreBlue,)
+          ],
+        ),
+      ),
+    );
+  }
+}
+
